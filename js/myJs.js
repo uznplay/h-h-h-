4f949702313e14b57174291e2c68f37468290ee7
uznplay <94210874+uznplay@users.noreply.github.com>
2022-04-05 13:21:03 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
   text1: "He luu cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text3: "Cậu vẫn iu iem duy có phải hơm nào 🌚❤️ ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ em duy rùi đó nha :v",
   text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text6: "Yêu em duy ơi là yêu <3",
+  text7: "Cậu có muốn tớ làm j cho cậu ko 😎 :vvvv",
+  text8: "Gửi cho tớ :>",
+  text9: "tớ muốn ngày ngày cậu mang xe tớ sang a2 để xe tớ vs xe duy gần nhau 😍😍😍",
+  text10: "Tớ biết mà ^^ tớ sẽ cố gắng hết sức",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Vậy chìu nay  với mai tớ sẽ làm nha cậu 🤔",
   text12: "Okii lunn <3",
 };
 
